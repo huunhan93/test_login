55e76dd01bc144e5fc6784b03ada0e5728529ffe
Hồ Hữu Nhân <nhanhh.dtp@vnpt.vn>
2024-09-16 10:07:13 +0700

    commit code

--- test_login/lib/screens/category/category_screen.dart
+++ test_login/lib/screens/category/category_screen.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:test_login/screens/category/provider/category_provider.dart';
-
 import '../category_detail/category_detail_screen.dart';
 import 'components/body.dart';
 
@@ -12,44 +9,30 @@ class CategoryScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    final categoryProvider = Provider.of<CategoryProvider>(context);
 
-    return FutureBuilder<void>(
-        future: categoryProvider.fetchCategories(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator();
-          }
-          else if (snapshot.hasError)
-          {
-            return Text('Có lỗi xảy ra');
-          }else{
-            return Scaffold(
-              appBar: AppBar(
-                title: const Text("Loại bài viết"),
-                actions: [
-                  IconButton(
-                      onPressed: (){
-                        Navigator.pushNamed(
-                            context,
-                            CategoryDetailScreen.routeName,
-                            arguments: {
-                              "titleName" : "Thêm mới ",
-                              "idCategory" : "",
-                            }
-                        ).then((_) => {
-
-                        });
-                      },
-                      icon: const Icon(Icons.add_box)
-                  )
-                ],
-              ),
-              body: Body(),
-              //bottomNavigationBar: CheckoutCard(),
-            );
-          }
-    });
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Loại bài viết"),
+        actions: [
+          IconButton(
+              onPressed: (){
+                Navigator.pushNamed(
+                    context,
+                    CategoryDetailScreen.routeName,
+                    arguments: {
+                      "titleName" : "Thêm mới ",
+                      "idCategory" : "",
+                    }
+                ).then((_) => {
+                  BodyState().getCategory()
+                });
+              },
+              icon: const Icon(Icons.add_box)
+          )
+        ],
+      ),
+      body: Body()
+    );
   }
 
 }
--- test_login/lib/screens/category/components/body.dart
+++ test_login/lib/screens/category/components/body.dart
@@ -2,9 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:provider/provider.dart';
-import 'package:test_login/screens/category/provider/category_provider.dart';
-
 import '../../../models/category.dart';
 import '../../../services/categoryService.dart';
 import '../../../size_config.dart';
@@ -19,111 +16,114 @@ class Body extends StatefulWidget {
 }
 
 class BodyState extends State<Body> {
-  //List<Category> listCategories = [Category(id: "", iconColor: 0, name: "",totalPost: 0)];
+  List<Category> listCategory = [];
 
-  // void getCategory(){
-  //   CategoryService().getCategory().then((value) => {
-  //     if(value.statusCode == 200){
-  //       setState(() {
-  //         listCategories = value.data!;
-  //       })
-  //     }
-  //   });
-  // }
+  void getCategory(){
+    CategoryService().getCategory().then((value) => {
+      if(value.statusCode == 200){
+        setState(() {
+          listCategory = value.data!;
+        })
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    getCategory();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<CategoryProvider>(
-      builder: (context, categoryProvider, child) {
-        return SafeArea(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-              child: ListView.builder(
-                itemCount: categoryProvider.categories.length,
-                itemBuilder: (context, index) => Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: Dismissible(
-                    key: Key(categoryProvider.categories[index].id.toString()),
-                    direction: DismissDirection.endToStart,
-                    onDismissed: (direction) {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: const Text("Xác nhận"),
-                            content: const Text("Bạn có chắc chắn muốn xóa mục này?"),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop(); // Đóng hộp thoại
-                                },
-                                child: const Text("Hủy"),
-                              ),
-                              TextButton(
-                                onPressed:  () async {
-                                  Navigator.of(context).pop(); // Đóng hộp thoại
-                                  bool result = await CategoryService().deleteCategory(categoryProvider.categories[index].id.toString());
-                                  if (result) {
-                                    setState(() {
-                                      categoryProvider.categories.removeAt(index);
-                                    });
-                                    // Hiển thị thông báo xóa thành công
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      const SnackBar(
-                                        content: const Text('Xóa thành công'),
-                                      ),
-                                    );
-                                  } else {
-                                    // Hiển thị thông báo xóa thất bại
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      SnackBar(
-                                        content: Text('Xóa thất bại'),
-                                      ),
-                                    );
-                                  }
-                                },
-                                child: Text("Xóa"),
-                              ),
-                            ],
-                          );
-                        },
-                      );
+    return SafeArea(
+        child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+            child: ListView.builder(
+              itemCount: listCategory.length,
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Dismissible(
+                  key: Key(listCategory[index].id.toString()),
+                  direction: DismissDirection.endToStart,
+                  onDismissed: (direction) {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text("Xác nhận"),
+                          content: const Text("Bạn có chắc chắn muốn xóa mục này?"),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop(); // Đóng hộp thoại
+                              },
+                              child: const Text("Hủy"),
+                            ),
+                            TextButton(
+                              onPressed:  () async {
+                                Navigator.of(context).pop(); // Đóng hộp thoại
+                                bool result = await CategoryService().deleteCategory(listCategory[index].id.toString());
+                                if (result) {
+                                  // setState(() {
+                                  //   listCategory.removeAt(index);
+                                  // });
+                                  // Hiển thị thông báo xóa thành công
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: const Text('Xóa thành công'),
+                                    ),
+                                  );
+                                } else {
+                                  // Hiển thị thông báo xóa thất bại
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: Text('Xóa thất bại'),
+                                    ),
+                                  );
+                                }
+                              },
+                              child: const Text("Xóa"),
+                            ),
+                          ],
+                        );
+                      },
+                    );
 
-                    },
-                    background: Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      decoration: BoxDecoration(
-                        color: const Color(0xFFFFE6E6),
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Row(
-                        children: [
-                          const Spacer(),
-                          SvgPicture.asset("assets/icons/Trash.svg"),
-                        ],
-                      ),
-                    ), child: CategoryItem(
-                    category: categoryProvider.categories[index],
-                    press: () async {
-                      await Navigator.pushNamed(
-                          context,
-                          CategoryDetailScreen.routeName,
-                          arguments: {
-                            "idCategory" : categoryProvider.categories[index].id.toString(),
-                            "titleName" : "Cập nhật "
-                          }
-                      ).then((_) {
-                        //getCategory();
-                      });
-                    },
-                  ),
-                    //child: CartCard(cart: demoCarts[index]),
-                  ),
+                  },
+                  background: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFFFE6E6),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Row(
+                      children: [
+                        const Spacer(),
+                        SvgPicture.asset("assets/icons/Trash.svg"),
+                      ],
+                    ),
+                  ), child: CategoryItem(
+                  category: listCategory[index],
+                  press: () async {
+                    await Navigator.pushNamed(
+                        context,
+                        CategoryDetailScreen.routeName,
+                        arguments: {
+                          "idCategory" : listCategory[index].id.toString(),
+                          "titleName" : "Cập nhật "
+                        }
+                    ).then((_) {
+                      getCategory();
+                    });
+                  },
+                ),
+                  //child: CartCard(cart: demoCarts[index]),
                 ),
               ),
-            )
-        );
-      },
+            ))
     );
   }
 }
